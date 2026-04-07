6ec44cf87b7fe172876a8a727aa47f6810294755
Marcio Bratfisch <djb278@txstate.edu>
2026-04-07 13:15:10 -0500

    Add runtime health tracking to CardInstance

--- Scripts/card_instance.gd
+++ Scripts/card_instance.gd
@@ -9,11 +9,13 @@ var data: CardData
 
 var zone: Zone = Zone.DECK
 var state: CardState = CardState.EXHAUSTED
+var current_health: int = 0
 
 
 func _init(_uid: int, _data: CardData):
 	uid = _uid
 	data = _data
+	current_health = _data.health
 
 
 func exhaust() -> void:
@@ -26,6 +28,18 @@ func awaken() -> void:
 
 func is_exhausted() -> bool:
 	return state == CardState.EXHAUSTED
+
+
+func take_damage(amount: int) -> void:
+	current_health = max(0, current_health - amount)
+
+
+func is_dead() -> bool:
+	return data.health > 0 and current_health <= 0
+
+
+func reset_health() -> void:
+	current_health = data.health
 
 
 func name() -> String:
