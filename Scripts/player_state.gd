0443ef32c4c816f77b156d946b6813aab5961749
Marcio Bratfisch <djb278@txstate.edu>
2026-04-07 22:36:59 -0500

    Fixed playerstate, added battlefield_units and corrected rune spending

--- Scripts/player_state.gd
+++ Scripts/player_state.gd
@@ -7,6 +7,7 @@ var deck: Array[CardInstance] = []
 var hand: Array[CardInstance] = []
 var board: Array[CardInstance] = []
 var trash: Array[CardInstance] = []
+var battlefield_units: Array[CardInstance] = []  # units moved to the contested battlefield
 var battlefields: Array[BattlefieldInstance] = []
 var picked_battlefield: BattlefieldInstance
 var rune_deck: Array[RuneInstance] = []
@@ -24,6 +25,8 @@ func _init(player_id: int):
 	id = player_id
 
 func pick_random_battlefield() -> void:
+	if battlefields.is_empty():
+		return
 	var index = randi() % battlefields.size()
 	battlefields[index].set_state(BattlefieldInstance.State.USED)
 	picked_battlefield = battlefields[index]
@@ -64,10 +67,10 @@ func spend_runes(selected_runes: Array[RuneInstance]) -> bool:
 			return false
 		if not rune_pool.has(rune):
 			return false
+		if rune.is_exhausted():
+			return false
 	for rune in selected_runes:
-		rune_pool.erase(rune)
-		rune.zone = RuneInstance.Zone.RUNE_DECK
-		rune_deck.append(rune)
+		rune.exhaust()
 	return true
 
 func recycle_runes_to_bottom(rune: RuneInstance) -> void:
